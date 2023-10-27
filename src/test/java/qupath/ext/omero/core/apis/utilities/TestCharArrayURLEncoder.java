de78a153041a99bda5ce6697904da60438533c24
Leo Leplat <60394504+Rylern@users.noreply.github.com>
2023-10-27 09:35:42 +0100

    Fixed import

--- src/test/java/qupath/ext/omero/core/apis/utilities/TestCharArrayURLEncoder.java
+++ src/test/java/qupath/ext/omero/core/apis/utilities/TestCharArrayURLEncoder.java
@@ -1,10 +1,10 @@
 package qupath.ext.omero.core.apis.utilities;
 
-import com.drew.lang.Charsets;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import java.nio.charset.StandardCharsets;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class TestCharArrayURLEncoder {
@@ -14,7 +14,7 @@ public class TestCharArrayURLEncoder {
         char[] text = "!#$%&'()+,/0123456789".toCharArray();
         char[] expectedEncodedText = "%21%23%24%25%26%27%28%29%2B%2C%2F0123456789".toCharArray();
 
-        char[] encodedText = CharArrayURLEncoder.encode(text, Charsets.UTF_8);
+        char[] encodedText = CharArrayURLEncoder.encode(text, StandardCharsets.UTF_8);
 
         Assertions.assertArrayEquals(expectedEncodedText, encodedText);
     }
